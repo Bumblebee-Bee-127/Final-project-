afdb8942de15480a617038de1f7f55f8b4c5b4f8
Bumblebee-Bee-127 <slavaalimova@yandex.ru>
2025-05-17 17:52:00 +0300

    Update pet_form.py

--- forms/pet_form.py
+++ forms/pet_form.py
@@ -79,7 +79,29 @@ def pet_form():
 </html>'''
     elif request.method == 'POST':
         print(request.form)
-        return Успешно!
+        return f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous">Успешно</script>
+    <link rel="stylesheet" type="text/css"
+          href="{url_for('static', filename='css/style.css')}">
+</head>   
+<body>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button type="button" class="btn btn-primary btn-lg" onclick="window.location = 'http://127.0.0.1:8080/login';">В каталог</button>
+  <button type="button" class="btn btn-secondary btn-lg" onclick="window.location = 'http://127.0.0.1:8080/register';">Выйти</button>
+
+</div>
+</body>
+</html>'''
 
 
 
